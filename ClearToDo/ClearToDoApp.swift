aabe09498df490191c215376f3c752e415a4b6a5
Ronak Shastri <r.shastri86@gmail.com>
2024-06-03 20:20:02 -0400

    Initial Commit

---
+++ ClearToDo/ClearToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClearToDoApp.swift
+//  ClearToDo
+//
+//  Created by Ronak Shastri on 2024-06-03.
+//
+
+import SwiftUI
+
+@main
+struct ClearToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
